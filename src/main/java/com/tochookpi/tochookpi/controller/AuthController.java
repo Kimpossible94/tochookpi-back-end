8487d42ab3d20bbac42a5d9c3e1b5db99345db7e
YoungBeom Kim <kimpossible94@naver.com>
2025-02-24 01:07:00 +0900

    * Feat: React Route 검증을 위한 컨트롤러 경로 추가

--- src/main/java/com/tochookpi/tochookpi/controller/AuthController.java
+++ src/main/java/com/tochookpi/tochookpi/controller/AuthController.java
@@ -26,13 +26,13 @@ public class AuthController {
         return ResponseEntity.ok().headers(headers).build();
     }
 
-    @PostMapping("verification-code")
+    @PostMapping("/verification-code")
     public ResponseEntity<Void> sendVerificationCode(@RequestBody PhoneVerificationDTO phoneVerificationDTO) {
         authService.sendVerificationCode(phoneVerificationDTO);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("verification-code/verify")
+    @PostMapping("/verification-code/verify")
     public ResponseEntity<String> verifyVerificationCode(@RequestBody PhoneVerificationDTO phoneVerificationDTO) {
         boolean isVerified = authService.verifyVerificationCode(phoneVerificationDTO);
         if (isVerified) {
@@ -40,5 +40,10 @@ public class AuthController {
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증번호가 잘못되었습니다.");
         }
+    }
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<Void> validateToken() {
+        return ResponseEntity.ok().build();
     }
 }
