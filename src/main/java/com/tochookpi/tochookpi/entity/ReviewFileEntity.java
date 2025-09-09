94f1e3bdb0b9b8c970b081b9d7c0a065b89b7b56
YoungBeom <Kimpossible94@naver.com>
2025-09-09 10:37:11 +0900

    Feat: 모임 리뷰 파일 논리적 삭제 컬럼 추가

--- src/main/java/com/tochookpi/tochookpi/entity/ReviewFileEntity.java
+++ src/main/java/com/tochookpi/tochookpi/entity/ReviewFileEntity.java
@@ -5,6 +5,10 @@ import com.tochookpi.tochookpi.dto.meeting.ReviewFileDTO;
 import com.tochookpi.tochookpi.enums.FileType;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "meeting_review_files")
@@ -13,6 +17,8 @@ import lombok.*;
 @Setter
 @Getter
 @Builder
+@SQLDelete(sql = "UPDATE meeting_review_files SET deleted_at = NOW() WHERE id = ?")
+@Where(clause = "deleted_at IS NULL")
 public class ReviewFileEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +34,9 @@ public class ReviewFileEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id", nullable = false)
     private MeetingReviewEntity review;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
     public ReviewFileDTO toDTO() {
         return ReviewFileDTO.builder()
